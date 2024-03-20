85c9c6e40207729a914f8a2aa5b48cc842868d95
jFlores <dotj@jBook.local>
2024-03-20 00:11:19 -0600

    added RNG function using the LFSR algorithm

--- labo1/labo1.c
+++ labo1/labo1.c
@@ -1,10 +1,26 @@
 // #include <pic12f683.h>
 #include <xc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdint.h>
 
 // typedef unsigned int word;
 // word __at 0x2007 __CONFIG = (_BODEN_OFF); 
 #pragma config BOREN = OFF  
-int NUM = 4;
+// int NUM = 4;
+
+
+// Linear Feedback Shift Register (LFSR) for pseudo-random number generation
+uint8_t lfsr_random() {
+    static uint16_t lfsr = 0xACE1u; // Example seed
+    unsigned lsb = lfsr & 1;  // Get least significant bit
+    lfsr >>= 1;               // Shift register
+    if (lsb)                  // Apply toggle mask
+        lfsr ^= 0xB400u;
+    return (uint8_t)(lfsr & 0x07); // Return only the lowest 3 bits
+}
+
 
 
 void dec2bin(int num, int binary[3]) {
@@ -21,21 +37,28 @@ void delay(unsigned int tiempo) {
 }
 
 
-
-
 void main() {
     TRISIO = 0x00; // Set GPIOs as outputs.
 
     while(1) {
         // Get binary representation of NUM.
-        int binary[3];
+        uint8_t random_number = lfsr_random() % 6 + 1; // Generate a number between 1 and 6
+
+
+
+        int binary[3]; // array to store generated number in 
+        // int NUM = 6;
+        int NUM = (int) random_number;
         dec2bin(NUM, binary);
 
         // Assign each bit to its corresponding pin.
-        GP0 = binary[0];
-        GP1 = binary[1];
-        GP2 = binary[2];
 
+        if (GP4 == 1){
+                GP0 = binary[0];
+                GP1 = binary[1];
+                GP2 = binary[2];
+                delay(250);
+        }
     }
 }
 
