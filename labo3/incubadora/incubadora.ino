d1f516d558ee5c45c0998736de8932f836ac23c3
rogerdanielpiovetgarcia@gmail.com <rogerdanielpiovetgarcia@gmail.com>
2024-04-27 14:02:46 -0600

    implemented LED alarms for temperature changes

--- labo3/incubadora/incubadora.ino
+++ labo3/incubadora/incubadora.ino
@@ -1,27 +1,40 @@
-#include <PCD8544.h>
+#include <PCD8544.h>   // https://github.com/carlosefr/pcd8544/tree/1.4.3
 #include <PID_v1_bc.h> // https://github.com/drf5n/Arduino-PID-Library
 
 double Setpoint, Entrada, Salida, x;
 //double Kp = 2, Ki = 5, Kd = 1;
-//double Kp = 17, Ki = .1, Kd = 2;
+//double Kp = 15, Ki = .1, Kd = 3;
 double Kp = 255, Ki = .0, Kd = 0; 
 PID myPID(&Entrada, &Salida, &Setpoint, Kp, Ki, Kd, P_ON_E, DIRECT);
+static PCD8544 lcd;
 
+int LCDSwitch = 0;
 const int OUT_PIN = 10;  				
 const int SETPOINT_PIN = A1;   	
-static PCD8544 lcd;
+const int LCD_ENABLE = 2;
+const int HI = 8;
+const int OP = 9;
+const int LO = 10;
 
 void setup()
 {
 	Serial.begin(115200);
+	// PID config
   	myPID.SetOutputLimits(0, 255);		// PID output in range 0-255
   	Setpoint = 36;					// 36 celsius as starting setpoint
   	myPID.SetMode(AUTOMATIC);
   	Entrada = simPlant(20.0 * (int)25/255); 	// start plant simulation at room temperature
   	Serial.println("Referencia Temperatura SalidaPID CalorQ");
-
+	
+	// LCD config
+	pinMode(LCD_ENABLE, INPUT);
 	lcd.begin();
 	lcd.clear();
+	
+	// LED alarms config
+	pinMode(HI, OUTPUT);
+	pinMode(OP, OUTPUT);
+	pinMode(LO, OUTPUT);
 }
 
 void loop()
@@ -36,24 +49,54 @@ void loop()
 		Setpoint = 4 * x/85 + 30;				// setpoint normalized to 30-42 celsius range
 	}							
 	
-	lcd.setCursor(0,0);
-	lcd.print("Temp de Op: ");
-	lcd.setCursor(0,1);
-	lcd.print(TempInc);
-	lcd.setCursor(30,1);
-	lcd.print(" Celsius");
-	lcd.setCursor(0,2);
-	lcd.print("Senal de Ctrl:");
-	lcd.setCursor(0,3);
-	lcd.print(TempWatts);
-	lcd.setCursor(30,3);
-	lcd.print(" Watts");
-	lcd.setCursor(0,4);
-	lcd.print("Temp deseada:");
-	lcd.setCursor(0,5);
-	lcd.print(Setpoint);
-	lcd.setCursor(30,5);
-	lcd.print(" Celsius");
+	LCDSwitch = digitalRead(LCD_ENABLE);
+	
+	// LCD screen switch
+	if (LCDSwitch == 0)
+	{
+		lcd.setCursor(0,0);
+		lcd.print("Temp de Op: ");
+		lcd.setCursor(0,1);
+		lcd.print(TempInc);
+		lcd.setCursor(30,1);
+		lcd.print(" Celsius");
+		lcd.setCursor(0,2);
+		lcd.print("Senal de Ctrl:");
+		lcd.setCursor(0,3);
+		lcd.print(TempWatts);
+		lcd.setCursor(30,3);
+		lcd.print(" Watts");
+		lcd.setCursor(0,4);
+		lcd.print("Temp deseada:");
+		lcd.setCursor(0,5);
+		lcd.print(Setpoint);
+		lcd.setCursor(30,5);
+		lcd.print(" Celsius");
+	}
+	else if (LCDSwitch == 1)
+	{
+		lcd.clear();
+	}
+	
+	// LED alarms
+	if (TempInc < 30)
+	{
+		digitalWrite(HI, LOW);
+		digitalWrite(OP, LOW);
+		digitalWrite(LO, HIGH);
+	}
+	else if (TempInc > 30 && TempInc < 42)
+	{
+		digitalWrite(HI, LOW);
+		digitalWrite(OP, HIGH);
+		digitalWrite(LO, LOW);
+	}
+	else if (TempInc > 42)
+	{
+		digitalWrite(HI, HIGH);
+		digitalWrite(OP, LOW);
+		digitalWrite(LO, LOW);
+	}
 }
 
 float simPlant(float Q) 
